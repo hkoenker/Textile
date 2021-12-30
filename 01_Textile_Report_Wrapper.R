daee86a723403b6d9c811047c7708b1e3f2aa686
hkoenker <hkoenker@gmail.com>
2021-12-30 11:22:42 -0500

    added MICS surveys to list

--- 01_Textile_Report_Wrapper.R
+++ 01_Textile_Report_Wrapper.R
@@ -1,32 +1,39 @@
 ### Script to run Textile analysis for all recent DHS/MIS surveys
 
 #textile_cty_list <- scan(text ="AOHR71 BJHR71 BFHR7A BUHR70 CDHR61 CGHR60 CIHR62 CMHR71 GAHR60 GHHR82 GMHR81 GNHR71 GYHR51 HTHR70 KEHR81 KHHR51 KMHR61 LBHR7A MDHR71 MLHR7H MMHR71 MWHR7Q MZHR7A NGHR7A NIHR61 NMHR61 PKHR71 PGHR70 RWHR7A SLHR7A SNHR8A STHR50  TDHR71 TGHR71 TLHR71 TZHR7Q UGHR7I VNHR52 ZMHR71 ZWHR71", what="")
+
+# manually adjust this list to include the most recent DHS, MIS, or MICS survey.
+
 textile_cty_list <- list(
   list(netfile="AOHR71_netfile.dta", country="Angola"),
   list(netfile="BJHR71_netfile.dta", country="Benin"),
   list(netfile="BFHR7A_netfile.dta", country="Burkina Faso"),
   list(netfile="BUHR70_netfile.dta", country="Burundi"),
-  list(netfile="CDHR61_netfile.dta", country="DRC"),
-  list(netfile="CIHR62_netfile.dta", country="Cote d'Ivoire"),
+  list(netfile="DRC18_netfile.dta", country="DRC"),
+  list(netfile="CAR19_netfile.dta", country="Central African Republic"),
+  list(netfile="CON15_netfile.dta", country="Republic of Congo"),
+  list(netfile="RCI16_netfile.dta", country="Cote d'Ivoire"),
   list(netfile="CMHR71_netfile.dta", country="Cameroon"),
   list(netfile="GHHR82_netfile.dta", country="Ghana"),
   list(netfile="GMHR81_netfile.dta", country="Gambia"),
   list(netfile="GNHR71_netfile.dta", country="Guinea"),
   list(netfile="KEHR81_netfile.dta", country="Kenya"),
   list(netfile="LBHR7A_netfile.dta", country="Liberia"),
-  list(netfile="MDHR71_netfile.dta", country="Madagascar"),
+  list(netfile="MDG18_netfile.dta", country="Madagascar"),
   list(netfile="MLHR7H_netfile.dta", country="Mali"),
   list(netfile="MWHR7Q_netfile.dta", country="Malawi"),
+  list(netfile="MAU15_netfile.dta", country="Mauritania"),
   list(netfile="MZHR7A_netfile.dta", country="Mozambique"),
   list(netfile="NGHR7A_netfile.dta", country="Nigeria"),
   list(netfile="RWHR81_netfile.dta", country="Rwanda"),
+  list(netfile="STP19_netfile.dta", country="Sao Tome & Principe"),
   list(netfile="SNHR8A_netfile.dta", country="Senegal"),
-  list(netfile="TDHR71_netfile.dta", country="Chad"),
+  list(netfile="TCH19_netfile.dta", country="Chad"),
   list(netfile="TGHR71_netfile.dta", country="Togo"),
   list(netfile="TZHR7Q_netfile.dta", country="Tanzania"),
   list(netfile="UGHR7I_netfile.dta", country="Uganda"),
   list(netfile="ZMHR71_netfile.dta", country="Zambia"),
-  list(netfile="ZWHR71_netfile.dta", country="Zimbabwe")
+  list(netfile="ZIM19_netfile.dta", country="Zimbabwe")
 )
 
 # textile_cty_list <- paste0(textile_cty_list,"_netfile.dta")
